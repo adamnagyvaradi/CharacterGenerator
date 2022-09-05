520049a78da1e42b9bffb1ee9fa826aa4841bf7c
Nagyvaradi Adam <adam.nagyvaradi@gmail.com>
2022-09-05 19:07:20 +0200

    Begining

---
+++ src/test/java/com/example/charactergenerator/CharacterGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.charactergenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharacterGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
