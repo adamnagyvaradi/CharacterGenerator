8ae3083f327e5782326300bbe9ff28ea5c5f3b06
Nagyandb <nagyandb@gmail.com>
2022-09-12 19:09:51 +0200

    Második controller létrehozása

--- src/main/java/com/example/charactergenerator/controller/SkillController.java
+++ src/main/java/com/example/charactergenerator/controller/SkillController.java
@@ -1,12 +1,14 @@
 package com.example.charactergenerator.controller;
 
 import com.example.charactergenerator.model.Skill;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Controller
 public class SkillController {
 
     @GetMapping(value = {"/sheet"})
