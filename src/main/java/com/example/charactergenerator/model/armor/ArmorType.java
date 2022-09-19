fd727ccbc1ed7203992cfceae3f70607f54beb98
Nagyandb <nagyandb@gmail.com>
2022-09-19 19:37:07 +0200

    Equipment: Armor

--- src/main/java/com/example/charactergenerator/model/armor/ArmorType.java
+++ src/main/java/com/example/charactergenerator/model/armor/ArmorType.java
@@ -1,15 +1,20 @@
 package com.example.charactergenerator.model.armor;
 
+import com.example.charactergenerator.model.AttributeType;
+
 public enum ArmorType {
 
-    LIGHT ("Light Armor"),
-    MEDIUM ("Medium Armor"),
-    HEAVY ("Heavy Armor");
+    LIGHT ("Light Armor", AttributeType.DEX),
+    MEDIUM ("Medium Armor", AttributeType.DEX),
+    HEAVY ("Heavy Armor", AttributeType.DEX);
 
     public final String name;
 
-    ArmorType(String name) {
+    private final AttributeType modifier;
+
+    ArmorType(String name, AttributeType modifier) {
         this.name = name;
+        this.modifier = modifier;
     }
 
     public String getName() {
