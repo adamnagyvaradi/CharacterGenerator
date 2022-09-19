0ad3a6a8ac4e593387239efbffd9b804a1b5159c
Nagyandb <nagyandb@gmail.com>
2022-09-19 19:14:04 +0200

    Equipment: Armor

--- src/main/java/com/example/charactergenerator/model/armor/Armor.java
+++ src/main/java/com/example/charactergenerator/model/armor/Armor.java
@@ -5,6 +5,36 @@ public class Armor {
     private String Name;
 
     private ArmorType armorType;
+
+    private byte armorValue;
+
     public Armor(String name) {
+    }
+
+    public Armor() {
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public ArmorType getArmorType() {
+        return armorType;
+    }
+
+    public void setArmorType(ArmorType armorType) {
+        this.armorType = armorType;
+    }
+
+    public byte getArmorValue() {
+        return armorValue;
+    }
+
+    public void setArmorValue(byte armorValue) {
+        this.armorValue = armorValue;
     }
 }
--- src/main/java/com/example/charactergenerator/model/armor/ArmorType.java
+++ src/main/java/com/example/charactergenerator/model/armor/ArmorType.java
@@ -2,8 +2,17 @@ package com.example.charactergenerator.model.armor;
 
 public enum ArmorType {
 
-    LIGHT,
-    MEDIUM,
-    HEAVY
+    LIGHT ("Light Armor"),
+    MEDIUM ("Medium Armor"),
+    HEAVY ("Heavy Armor");
 
+    public final String name;
+
+    ArmorType(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
