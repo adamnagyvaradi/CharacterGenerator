ce39a1cb8f0988cb9fa77792fcc7e894a5d48fc7
Raw3n12 <aron.peccol@gmail.com>
2022-09-21 17:56:00 +0200

    Challangerating implementation, proficiency and Type

---
+++ src/main/java/com/example/charactergenerator/model/CharacterType.java
@@ -0,0 +1,20 @@
+package com.example.charactergenerator.model;
+
+public enum CharacterType {
+
+    ABERRATION,
+    BEAST,
+    CELESTIAL,
+    CONSTRUCT,
+    DRAGON,
+    ELEMENTAL,
+    FEY,
+    FIEND,
+    GIANT,
+    HUMANOID,
+    MONSTROSITY,
+    OOZE,
+    PLANT,
+    UNDEAD
+
+}
--- src/main/java/com/example/charactergenerator/model/Skill.java
+++ src/main/java/com/example/charactergenerator/model/Skill.java
@@ -8,32 +8,25 @@ public class Skill {
     public Skill() {
 
     }
-
     public Skill(SkillType skillType, Character character) {
         this.skillType = skillType;
         this.character = character;
     }
-
     public String getName() {
         return skillType.getName();
     }
-
     public boolean isProficiency() {
         return proficiency;
     }
-
     public void setProficiency(boolean proficiency) {
         this.proficiency = proficiency;
     }
-
     public AttributeType getModifier() {
         return skillType.getModifier();
     }
-
     public byte getBonus() {
         return character.getAttributeBonus(getModifier());
     }
-
     public String getBonusSign(){
         return getBonus() > 0 ? "+" : "";
     }
