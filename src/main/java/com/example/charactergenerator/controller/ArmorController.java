2f6f93614656e592d192f9d135e5615d914a47fa
Nagyandb <nagyandb@gmail.com>
2022-09-21 16:54:24 +0200

    Equipment: Armor

--- src/main/java/com/example/charactergenerator/controller/ArmorController.java
+++ src/main/java/com/example/charactergenerator/controller/ArmorController.java
@@ -1,7 +1,10 @@
 package com.example.charactergenerator.controller;
 
+import com.example.charactergenerator.service.ArmorService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ArmorController {
+
+    private ArmorService armorService;
 }
