e18d5679d46a7ada9d645e8b9bfd36d3587a543c
Nagyandb <nagyandb@gmail.com>
2022-09-22 17:09:18 +0200

    Equipment: Armor

--- src/main/java/com/example/charactergenerator/controller/ArmorController.java
+++ src/main/java/com/example/charactergenerator/controller/ArmorController.java
@@ -1,12 +1,15 @@
 package com.example.charactergenerator.controller;
 
 import com.example.charactergenerator.dto.CharacterDto;
+import com.example.charactergenerator.model.Armor;
 import com.example.charactergenerator.model.Character;
 import com.example.charactergenerator.service.ArmorService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 @Controller
 public class ArmorController {
@@ -15,7 +18,7 @@ public class ArmorController {
 
     @GetMapping(value = {"/character/{armor}"})
     public String getArmorbyCharacter(Model model){
-
+        //List<Armor> allArmors = ArmorService.
 
 
         return "character";
