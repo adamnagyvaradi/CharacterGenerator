7ba4d7a4453c299a0a33f0bb42fb6ef7ffb83cf1
Nagyandb <nagyandb@gmail.com>
2022-09-22 19:38:02 +0200

    Equipment: Armor

--- src/main/java/com/example/charactergenerator/service/ArmorService.java
+++ src/main/java/com/example/charactergenerator/service/ArmorService.java
@@ -11,15 +11,7 @@ public class ArmorService {
 
     private ArmorRepository armorRepository;
 
-    Character character1 = new Character("Padded", ArmorType.LIGHT, (byte) 0);
-    Character character2 = new Character("Leather", ArmorType.LIGHT, (byte) 1);
-    Character character3 = new Character("Studded leather", ArmorType.LIGHT, (byte) 3);
-    Character character4 = new Character("Chain shirt", ArmorType.MEDIUM, (byte) 0);
-    Character character5 = new Character("Scale mail", ArmorType.MEDIUM, (byte) 1);
-    Character character6 = new Character("Breastplate", ArmorType.MEDIUM, (byte) 3);
-    Character character7 = new Character("Ring mail", ArmorType.HEAVY, (byte) 0);
-    Character character8 = new Character("Chain mail", ArmorType.HEAVY, (byte) 1);
-    Character character9 = new Character("Splint", ArmorType.HEAVY, (byte) 3);
+    //Character character1 = new Character("A", 10, 1,
 
 
 }
