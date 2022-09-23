47cd8f99eadcfd91d32a03a5676724278a716764
Nagyandb <nagyandb@gmail.com>
2022-09-23 03:11:53 +0200

    Equipment: Armor

--- src/main/java/com/example/charactergenerator/service/ArmorService.java
+++ src/main/java/com/example/charactergenerator/service/ArmorService.java
@@ -12,6 +12,14 @@ public class ArmorService {
     private ArmorRepository armorRepository;
 
     Character goblin = new Character("Hobgoblin", (byte) 10, (short) 7, (byte) 8, (byte) 4, (byte) 4, (byte) 4, (byte) 8, (byte) 11, (byte) 4, (byte) 4);
+    Character skeleton = new Character("Giant Skeleton", (byte) 10, (short) 7, (byte) 8, (byte) 4, (byte) 4, (byte) 4, (byte) 8, (byte) 11, (byte) 4, (byte) 4);
+    Character aarakocra = new Character("Aarakocra", (byte) 10, (short) 7, (byte) 8, (byte) 4, (byte) 4, (byte) 4, (byte) 8, (byte) 11, (byte) 4, (byte) 4);
+    Character bandit = new Character("Bandit", (byte) 10, (short) 7, (byte) 8, (byte) 4, (byte) 4, (byte) 4, (byte) 8, (byte) 11, (byte) 4, (byte) 4);
+    Character bandit2 = new Character("Banit Captain", (byte) 10, (short) 7, (byte) 8, (byte) 4, (byte) 4, (byte) 4, (byte) 8, (byte) 11, (byte) 4, (byte) 4);
+    Character inquisitor = new Character("Drow Inquisitor", (byte) 10, (short) 7, (byte) 8, (byte) 4, (byte) 4, (byte) 4, (byte) 8, (byte) 11, (byte) 4, (byte) 4);
+    Character grimlock = new Character("Grimlock", (byte) 11, (short) 11, (byte) 30 , (byte) 2, (byte) 16, (byte) 12, (byte) 12, (byte) 9, (byte) 8, (byte) 6);
+    Character merfolk = new Character("Merfolk", (byte) 11, (short) 11, (byte) 10, (byte) 2, (byte) 10, (byte) 13, (byte) 12, (byte) 11, (byte) 11, (byte) 12);
+    Character tribalWarrior = new Character("Tribal Warrior", (byte) 12, (short) 11, (byte) 30, (byte) 2, (byte) 13, (byte) 11, (byte) 12, (byte) 8, (byte) 11, (byte) 8);
 
 
 }
