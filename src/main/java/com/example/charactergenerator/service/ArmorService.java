0af63586cd016f43c58a9fb6d582149b62756a4f
Nagyandb <nagyandb@gmail.com>
2022-09-23 02:29:47 +0200

    Equipment: Armor

--- src/main/java/com/example/charactergenerator/service/ArmorService.java
+++ src/main/java/com/example/charactergenerator/service/ArmorService.java
@@ -11,7 +11,7 @@ public class ArmorService {
 
     private ArmorRepository armorRepository;
 
-    //Character character1 = new Character("A", 10, 1,
+    Character goblin = new Character("Hobgoblin", (byte) 10, (short) 7, (byte) 8, (byte) 4, (byte) 4, (byte) 4, (byte) 8, (byte) 11, (byte) 4, (byte) 4);
 
 
 }
