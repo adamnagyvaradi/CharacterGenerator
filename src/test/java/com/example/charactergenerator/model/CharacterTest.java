5bf2f1273b6caee705f572f00cec0268a8372bfc
bertalan83 <e.bertalan96@gmail.com>
2022-10-03 17:48:51 +0200

    berci: creating a character model test

--- src/test/java/com/example/charactergenerator/model/CharacterTest.java
+++ src/test/java/com/example/charactergenerator/model/CharacterTest.java
@@ -1,13 +1,35 @@
 package com.example.charactergenerator.model;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CharacterTest {
 
+    Character testChar;
+
+    @BeforeEach
+    void createTestChar() {
+        testChar =
+                new Character("Test", 1, 1, 1,
+                        1, 1, 1, 1,
+                        1, 1, 1,
+                        CharacterType.ABERRATION,
+                        List.of(SkillType.ACROBATICS,SkillType.RELIGION, SkillType.ARCANA));
+    }
+
+
     @Test
     void assignSlots() {
+
+    }
+
+    @Test
+    void getAttributeBonusTest() {
+
 
     }
 }
