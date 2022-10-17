67e64e97e391eec5d416fdbc8de1eb8e89adab6e
Raw3n12 <aron.peccol@gmail.com>
2022-10-17 18:23:29 +0200

    Merge remote-tracking branch 'origin/meregeTest' into meregeTest

--- src/main/java/com/example/charactergenerator/controller/EncounterController.java
+++ src/main/java/com/example/charactergenerator/controller/EncounterController.java
@@ -47,22 +47,13 @@ public class EncounterController {
     }
 
     @GetMapping("/encounter/builder")
-    public String showEncounterBuilder(Model model,
-                                       @RequestParam(required = false, name = "keyword", defaultValue = "") String keyword){
-        if (!encounterService.isEncounterEditable()){
-            return "redirect:/encounter/character";
-        }
+    public String searchCharacter(Model model,
+                                  @RequestParam(required = false, name = "characterName",defaultValue ="") String characterName,
+                                  @RequestParam(required = false, name = "characterType") String characterType,
+                                  @RequestParam(required = false, name = "challengeRating") Byte challengeRating) {
 
-        List<Character> characters;
-
-        if(keyword.isBlank()){
-            characters = characterService.findAll();
-        }else{
-            characters = characterService.findAllByNameContains(keyword);
-        }
-
-        model.addAttribute("characters",characters);
-        model.addAttribute("charactersCart",encounterService.getAllCharacter());
+        List<Character> characterList = characterService.filterBy(characterName,characterType, challengeRating);
+        model.addAttribute("characters",characterList);
 
         return "encounter/builder";
     }
